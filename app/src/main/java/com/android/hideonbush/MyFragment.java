a334928179f771d3ca73c55098639080e2de6c6e
meng.zhang <meng.zhang@ihandysoft.com>
2019-07-11 13:03:08 +0800

    update

---
+++ app/src/main/java/com/android/hideonbush/MyFragment.java
@@ -0,0 +1,34 @@
+package com.android.hideonbush;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * <pre>
+ *     author : philip-knight
+ *     e-mail : meng.zhang@ihandysoft.com
+ *     time   : 2019/07/08
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class MyFragment extends Fragment {
+
+    private String content;
+    public MyFragment(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content,container,false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+}
